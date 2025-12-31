38c81f933506710e4ad1dd41d031e941a9a71e70
Alex Medina <alexmedval2@gmail.com>
2025-12-31 09:50:15 -0600

    create new arrivals section

---
+++ components/ui/WaitingHomeDogCards.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+type WaitingHomeDogCardsProps = {
+  src: string;
+  name: string;
+  subtitle: string;
+  breed: string;
+  age: string;
+};
+
+export default function WaitingHomeDogCards({
+  src,
+  name,
+  subtitle,
+  breed,
+  age,
+}: WaitingHomeDogCardsProps) {
+  return (
+    <>
+      <div>
+        <Image
+          src={src}
+          alt={`Image of ${name}, a dog waiting for a home`}
+          width={300}
+          height={300}
+          className="rounded-t-2xl"
+        />
+        <div className=" bg-gray-100 p-4 rounded-b-2xl">
+          <h5 className="text-xl font-bold text-black">{name}</h5>
+          <p className="text-gray-500">{subtitle}</p>
+          <p className="text-gray-700 mt-2 font-bold">
+            Raza: <span className="font-semibold text-gray-500">{breed}</span>
+          </p>
+          <p className="text-gray-700 font-bold">
+            Edad: <span className="font-semibold text-gray-500">{age}</span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
